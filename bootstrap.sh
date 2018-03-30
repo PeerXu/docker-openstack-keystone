53789c86c96f7458fd49ece80bd890af37168ed8
Peer Xu <pppeerxu@gmail.com>
2018-03-30 15:22:27 +0800

    :bug: remove uwsgi from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,6 @@ mkdir -p /var/lib/keystone
 keystone-manage db_sync
 keystone-manage fernet_setup --keystone-user root --keystone-group root
 
-uwsgi --http 0.0.0.0:35357 --wsgi-file $(which keystone-wsgi-admin) &
-uwsgi --http 0.0.0.0:5000 --wsgi-file $(which keystone-wsgi-public) &
-
 keystone-manage bootstrap --bootstrap-username admin \
 		--bootstrap-password 'admin123!@#' \
 		--bootstrap-project-name admin \
